767f3eaf812c87f8b62729e401f186308effae33
minber <MindaugasBer@hotmail.lt>
2018-09-04 12:11:36 +0300

    C Sharp Uzduotis

---
+++ C_sharp_uzduotis/C_sharp_uzduotis/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_uzduotis
+{
+    public class Program
+    {
+        private const string ReadingFile = "readText.txt";
+        private const string WritingFile = "writeText.txt";
+
+        public static void Main(string[] args)
+        {
+            
+            var resultLines = new List<string>();
+            var size = InputOfLettersLengthInLine();
+            var lines = ReadingTextFromFile(ReadingFile);
+            resultLines = lines.Select(SplitingLineIntoWords)
+                .Aggregate(resultLines, (current, words) => SplitingWordsInLines(words, current, size));
+            WritingWordsToFile(resultLines, WritingFile);
+        }
+
+        private static int InputOfLettersLengthInLine()
+        {
+            int size;
+            Console.WriteLine("Įveskite raidžių kiekį eilutėje: ");
+            var input = Console.ReadLine();
+            while (!int.TryParse(input, out size))
+            {
+                Console.WriteLine("Įvedėte ne skaičių");
+                Console.WriteLine("Įveskite raidžių kiekį eilutėje: ");
+                input = Console.ReadLine();
+            }
+            return size;
+        }
+
+        public static string[] ReadingTextFromFile(string filename) 
+        {
+            return System.IO.File.ReadAllLines(@filename, Encoding.UTF8); 
+        }
+
+        public static string[] SplitingLineIntoWords(string line)
+        {
+            return line.Split(new string[] {" ", "."}, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static List<string> SplitingWordsInLines(string[] words, List<string> resultLines, int size)
+        { 
+            string formatedLine = "";
+
+            int leftSpaceInLine = LeftSpaceInLineDependsOnLastLineOfList(resultLines, size);
+
+
+            foreach (var word in words)
+            {
+                if (word.Length <= leftSpaceInLine)
+                   AddWordToLastLine(ref resultLines, word, ref leftSpaceInLine);
+                
+                else if (word.Length > leftSpaceInLine)
+                {
+                    leftSpaceInLine = size;
+                    if (word.Length > size)
+                    {
+                        SplitsWordInLinesIfItIsTooLong(word, ref formatedLine, size, ref resultLines);
+                        ChecksIfThereIsAnySpaceLeftInLine(formatedLine, size, ref leftSpaceInLine, ref resultLines);
+                    }
+                    else
+                        AddsANewLine(ref resultLines, word, ref leftSpaceInLine);
+                }
+            }
+            return resultLines;
+        }
+
+        public static int LeftSpaceInLineDependsOnLastLineOfList(List<string> resultLines, int size)
+        {
+            if (resultLines.Count != 0)
+            {
+                return size - resultLines[resultLines.Count - 1].Length;
+            }
+            return size;
+        }
+
+        public static void AddsANewLine(ref List<string> resultLines, string word, ref int leftsize)
+        {
+            resultLines.Add(word);
+            leftsize -= word.Length;
+        }
+
+        public static void ChecksIfThereIsAnySpaceLeftInLine(string formatedLine, int size, ref int leftsize, ref List<string> resultLines)
+        {
+            if (formatedLine.Length != size)
+            {
+                leftsize = size - formatedLine.Length;
+            }
+        }
+
+        public static void SplitsWordInLinesIfItIsTooLong(string word, ref string formatedLine, int size,
+            ref List<string> resultLines)
+        {
+            int startindex = 0;
+
+            while (startindex < word.Length)
+            {
+                formatedLine = word.Substring(startindex, word.Length < startindex + size
+                    ? word.Length - startindex : size);
+                startindex += size;
+                resultLines.Add(formatedLine);
+            }
+        }
+
+        public static void AddWordToLastLine(ref List<string> resultLines, string word, ref int leftsize)
+        {
+            if (resultLines.Count != 0)
+                if(resultLines.Last() == "")
+                    resultLines[resultLines.Count - 1] += word;
+                else
+                    resultLines[resultLines.Count - 1] += " " + word;
+            else
+                resultLines.Add(word);
+            leftsize -= word.Length;
+        }
+
+        public static void WritingWordsToFile(List<string> results, string filename)
+        {
+            using (var file = new System.IO.StreamWriter(@filename))
+            {
+                foreach (var c in results)
+                {
+                    file.WriteLine(c);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ C_sharp_uzduotis/C_sharp_uzduotis_tests/UnitTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C_sharp_uzduotis;
+namespace C_sharp_uzduotis_tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Check_If_It_Reads_Correctly_From_File()
+        {
+            var testLines = new String[] {"žodis žodis žodis"};
+            var lines = Program.ReadingTextFromFile("readText.txt");
+            Assert.IsNotNull(lines);
+            Assert.AreEqual(testLines[0], lines[0]);
+        }
+
+
+        [TestMethod]
+        public void Check_if_Line_Splits_Into_Words()
+        {
+            string testLine = "Lorem ipsum fames hac arcu enim.";
+            string[] testWords = new string[] {"Lorem", "ipsum", "fames", "hac", "arcu", "enim"};
+            string[] words = Program.SplitingLineIntoWords(testLine);
+
+            CollectionAssert.AreEqual(testWords, words);
+        }
+
+        [TestMethod]
+        public void Check_If_It_Adds_Word_To_Last_Line_When_List_Is_Not_Empty()
+        {
+            var resultLines = new List<string>();
+            string word = "Lorem";
+            resultLines.Add("");
+            int leftsize = 6;
+            Program.AddWordToLastLine(ref resultLines, word, ref leftsize);
+            Assert.AreEqual(word, resultLines.Last());
+            Assert.AreEqual(1, leftsize);
+        }
+
+        [TestMethod]
+        public void Check_When_List_Empty_Add_New_Line()
+        {
+            var resultLines = new List<string>();
+            string word = "Lorem";
+            int leftsize = 6;
+            Program.AddWordToLastLine(ref resultLines, word, ref leftsize);
+            Assert.AreEqual(word, resultLines.Last());
+            Assert.AreEqual(1, leftsize);
+        }
+
+        [TestMethod]
+        public void Check_When_Word_Is_Longer_Than_Size_Then_Split_Into_Lines()
+        {
+            string word = "Lorem";
+            var resultLines = new List<string>();
+            var expectedResult = new List<string>() {"Lo", "re", "m"};
+            int size = 2;
+            string formatedLine = "";
+            Program.SplitsWordInLinesIfItIsTooLong(word,ref formatedLine, size, ref resultLines);
+
+            CollectionAssert.AreEqual(expectedResult, resultLines);
+            Assert.AreEqual("m", formatedLine);
+        }
+
+        [TestMethod]
+        public void Check_When_Line_Length_Less_Than_Size_After_Spliting_Word_Add_Space_To_Last_Line()
+        {
+            string formatedLine = "test";
+            int leftsize = 0;
+            int size = 5;
+            var resultLines = new List<string>() {"test"};
+            Program.ChecksIfThereIsAnySpaceLeftInLine(formatedLine,size,ref leftsize, ref resultLines);
+            Assert.AreEqual("test", resultLines.Last());
+            Assert.AreEqual(1, leftsize);
+        }
+
+        [TestMethod]
+        public void Check_If_New_Line_Have_Been_Added_With_Word()
+        {
+            var resultLines = new List<string>() { "test" };
+            string word = "Lorem";
+            int leftsize = 5;
+            Program.AddsANewLine(ref resultLines, word, ref leftsize);
+            Assert.AreEqual("Lorem",resultLines[resultLines.Count - 1]);
+            Assert.AreEqual(0, leftsize);
+        }
+
+        [TestMethod]
+        public void Check_If_Results_Are_Correct_With_Given_Words_Array()
+        {
+            string line = "šiuolaikiškas ir mano žodis";
+            int size = 7;
+            string[] words = Program.SplitingLineIntoWords(line);
+            var resultLines = new List<string>();
+            var expectedResults = new List<string>() {"šiuolai", "kiškas", "ir mano", "žodis"};
+            resultLines = Program.SplitingWordsInLines(words, resultLines, size);
+            CollectionAssert.AreEqual(expectedResults, resultLines);
+        }
+
+        [TestMethod]
+        public void Check_If_Results_Are_Correct_With_Given_Line_Array()
+        {
+            var resultLines = new List<string>();
+            var testData = new String[] {"žodis žodis žodis", "šiuolaikiškas ir mano žodis"};
+            var expectedResults = new List<string>() { "žodis žodis", "žodis", "šiuolaikiškas", "ir mano žodis" };
+            var size = 13;
+            resultLines = testData.Select(Program.SplitingLineIntoWords)
+               .Aggregate(resultLines, (current, words) => Program.SplitingWordsInLines(words, current, size));
+            CollectionAssert.AreEqual(expectedResults, resultLines);
+        }
+
+       [TestMethod]
+        public void Check_If_Writing_To_File_Works()
+        {
+            var resultLines = new List<string>() { "test" };
+            Program.WritingWordsToFile(resultLines, "writeText.txt");
+            string[] result = Program.ReadingTextFromFile("writeText.txt");
+            Assert.AreEqual("test", result[0]);
+        }
+
+       
+
+    }
+}
